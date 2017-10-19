ebd518105ea703ad6a2a9ad318b18be877f6ec4f
frankhjwx <frankhjwx@gmail.com>
2017-10-19 16:29:19 +0800

    Second Commit

---
+++ rpc/client/client test.bat
@@ -0,0 +1,3 @@
+@echo off
+FOR /L %%i in (1,1,10) do client 45.78.30.208
+pause
--- rpc/client/client.go
+++ rpc/client/client.go
@@ -3,12 +3,14 @@ package main
     import (
         "fmt"
         "log"
+		"net"
         "net/rpc"
         "os"
 		"time"
     )
 	
 	type Args struct {
+		A string
 	}
 
     func main() {
@@ -22,25 +24,44 @@ package main
         if err != nil {
             log.Fatal("dialing:", err)
         }
-        
-		args := Args{}
-        var reply time.Time
+		
+		addrs, err := net.InterfaceAddrs()
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		var ipaddr string;
+		for _, address := range addrs {
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					ipaddr = ipnet.IP.String()
+				}
+			}
+		}
+	
+		args := Args{ipaddr}
+		var reply time.Time
 		t_start := time.Now()
-        err = client.Call("Arith.GetTime", args, &reply)
+		err = client.Call("Arith.GetTime", args, &reply)
 		t_end := time.Now()
-        if err != nil {
-            log.Fatal("arith error:", err)
-        }
+		if err != nil {
+			log.Fatal("arith error:", err)
+		}
 		
 		t_dur := t_end.Sub(t_start)  //  calculate the duration time
-        fmt.Printf("The time got from the server: ");
-		fmt.Println(reply)
-		fmt.Printf("The duration time: ");
-		fmt.Println(t_dur)
-		
-		fmt.Printf("The accurate time on the client: ");
-		cur_time := reply.Add(t_dur/2) //   Because the time spent is bidirectional, we need to divide it by 2.
-		fmt.Println(cur_time)
+		if !reply.IsZero() {
+			fmt.Printf("The time got from the server: ");
+			fmt.Println(reply)
+			fmt.Printf("The time of transmission: ");
+			fmt.Println(t_dur)
+			fmt.Printf("The accurate time on the client: ");
+			cur_time := reply.Add(t_dur/2) //   Because the time spent is bidirectional, we need to divide it by 2.
+			fmt.Println(cur_time)
+		} else {
+			fmt.Printf("The server rejected the quest.\n")
+		}
         
 
     }
